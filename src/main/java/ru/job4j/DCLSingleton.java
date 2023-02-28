e530982707220d7eae4e0c82bdf0d39b48d1b84b
Mars <knownasmars@gmail.com>
2023-02-28 15:14:45 +0300

    2. Модель памяти Java [#267917 #395909]

--- src/main/java/ru/job4j/DCLSingleton.java
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -1,11 +1,16 @@
 package ru.job4j;
 
 public final class DCLSingleton {
+
     private static volatile DCLSingleton inst;
 
     public static DCLSingleton instOf() {
         if (inst == null) {
-            inst = new DCLSingleton();
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
         }
         return inst;
     }
