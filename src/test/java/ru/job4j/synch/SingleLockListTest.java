8632ac7e941762635f04fac14a4677b7a3d14f41
Mars <knownasmars@gmail.com>
2023-03-01 18:16:23 +0300

    4. ThreadSafe динамический список [#1105]

--- src/test/java/ru/job4j/synch/SingleLockListTest.java
+++ src/test/java/ru/job4j/synch/SingleLockListTest.java
@@ -10,7 +10,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SingleLockListTest {
-
     @Test
     public void whenIt(){
         var init = new ArrayList<Integer>();
