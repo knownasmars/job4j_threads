3c1617e2e98f438161ad5e5ece0359867c2c0f20
Mars <knownasmars@gmail.com>
2023-03-03 15:38:40 +0300

    3. Junit тест для блокирующей очереди. [#68589 #397257]

--- src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/blocking/SimpleBlockingQueueTest.java
@@ -34,7 +34,6 @@ public class SimpleBlockingQueueTest {
                         try {
                             buffer.add(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
