c831f9724b6fe31def1cb4216cfd26172d6973e6
Mars <knownasmars@gmail.com>
2023-03-08 19:04:16 +0300

    1. Реализовать ThreadPool [#1099 #398341]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -1,5 +1,6 @@
 package ru.job4j.pool;
 
+import ru.job4j.CASCount;
 import ru.job4j.blocking.SimpleBlockingQueue;
 
 import java.util.LinkedList;
@@ -9,16 +10,19 @@ public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks;
 
-    public ThreadPool() throws InterruptedException {
+    public ThreadPool() {
         int size = Runtime.getRuntime().availableProcessors();
         tasks = new SimpleBlockingQueue<>(size);
         for (int i = 0; i < size; i++) {
             threads.add(new Thread(
                     () -> {
-                        try {
-                            tasks.poll().run();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                        while (!Thread.currentThread().isInterrupted()) {
+                            try {
+                                tasks.poll().run();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                e.printStackTrace();
+                            }
                         }
                     })
             );
@@ -26,17 +30,23 @@ public class ThreadPool {
         threads.forEach(Thread::start);
     }
 
-    public void work(Runnable job) {
-        try {
-            tasks.offer(job);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
     }
 
     public void shutdown() {
         threads.forEach(thread -> {
             Thread.currentThread().interrupt();
         });
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        CASCount casCount = new CASCount();
+        for (int i = 0; i < 10; i++) {
+            threadPool.work(casCount::increment);
+        }
+        System.out.println(casCount.get());
+        threadPool.shutdown();
     }
 }
