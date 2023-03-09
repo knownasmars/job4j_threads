96fb46763553bbb866e5a37d0ac164b43d0e5ba4
Mars <knownasmars@gmail.com>
2023-03-09 22:59:26 +0300

    3. ForkJoinPool [#315067 #398563]

--- src/main/java/ru/job4j/fork/IndexFinder.java
+++ src/main/java/ru/job4j/fork/IndexFinder.java
@@ -1,5 +1,6 @@
 package ru.job4j.fork;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class IndexFinder<T> extends RecursiveTask<Integer> {
@@ -9,29 +10,39 @@ public class IndexFinder<T> extends RecursiveTask<Integer> {
     private final T el;
 
     public IndexFinder(T[] array, int from, int to, T el) {
-            this.array = array;
-            this.from = from;
-            this.to = to;
-            this.el = el;
+        this.array = array;
+        this.from = from;
+        this.to = to;
+        this.el = el;
+    }
+
+    public static <T> T finder (IndexFinder<T> index) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return (T) forkJoinPool.invoke(index);
+    }
+
+    private int linear() {
+        int rsl = -1;
+        for (int i = from; i <= to; i++) {
+            if (array[i].equals(el)) {
+                rsl = i;
+            }
         }
+        return rsl;
+    }
 
     @Override
     protected Integer compute() {
         if (to - from <= 10) {
-            for (int i = from; i <= to; i++) {
-                if (array[i].equals(el)) {
-                    return i;
-                }
-            }
-            return -1;
+            return linear();
         }
         var mid = (from + to) / 2;
         IndexFinder<T> leftFind = new IndexFinder<>(array, from, mid, el);
         IndexFinder<T> rightFind = new IndexFinder<>(array, mid + 1, to, el);
-        leftFind.fork();
-        rightFind.fork();
-        int e1 = leftFind.join();
-        int e2 = rightFind.join();
-        return Math.max(e1, e2);
+        finder(leftFind);
+        finder(rightFind);
+        int leftElement = leftFind.join();
+        int rightElement = rightFind.join();
+        return Math.max(leftElement, rightElement);
     }
 }
